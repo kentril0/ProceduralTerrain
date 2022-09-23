72aa48713a001e1912452e236fb9bcdef151b087
kentril0 <kentril.despair@gmail.com>
2022-09-23 18:46:27 +0200

    Updated perlin noise, more params to fractal noise

--- src/scene/FractalNoise.h
+++ src/scene/FractalNoise.h
@@ -10,40 +10,62 @@
 
 /** 
  * @brief Fractal noise generator based on *Perlin Noise*
- *  @see Sascha Willems. Vulkan. https://github.com/SaschaWillems/Vulkan.
  */
 template <typename T>
 class FractalNoise
 {
 public:
-    FractalNoise(const PerlinNoise<T>& perlinNoise)
+    /**
+     * @param scale Scales the sample
+     * @param offset Offsets the sample
+     * @param octaves Number of octaves
+     * @param gain Scales amplitude, influence of a successive octave
+     * @param lacunarity Scales frequency with each successive octave
+     */
+    FractalNoise(const PerlinNoise<T>& perlinNoise,
+                 T scale = (T)1,
+                 T offset = (T)0,
+                 uint32_t octaves = 6,
+                 T gain = (T)0.5,
+                 T lacunarity = (T)2)
         : m_PerlinNoise(perlinNoise),
-          m_Octaves(6),
-          m_Persistence((T)0.5) {}
+          m_Scale(scale),
+          m_Offset(offset),
+          m_Octaves(octaves),
+          m_Gain(gain),
+          m_Lacunarity(lacunarity) {}
 
+    /** @return 3D Fractal noise value in [-1,1] */
     T Noise(T x, T y, T z)
     {
-    	T sum = 0;
-    	T frequency = (T)1;
-    	T amplitude = (T)1;
-    	T max = (T)0;
-    	for (uint32_t i = 0; i < m_Octaves; i++)
-    	{
-    		sum += m_PerlinNoise.Noise(x * frequency,
-                                       y * frequency,
-                                       z * frequency) * amplitude;
-    		max += amplitude;
-    		amplitude *= m_Persistence;
-    		frequency *= (T)2;
-    	}
+        T sum = 0;
+        T max = (T)0;
+        T frequency = (T)1;
+        T amplitude = (T)1;
 
-    	sum = sum / max;
-    	return (sum + (T)1.0) / (T)2.0;
+        for (uint32_t i = 0; i < m_Octaves; ++i)
+        {
+            T noiseVal = m_PerlinNoise.Noise(
+                x / m_Scale * frequency + m_Offset,
+                y / m_Scale * frequency + m_Offset,
+                z / m_Scale * frequency + m_Offset); // * (T)2 - (T)1 ;
+
+            sum += noiseVal * amplitude;
+            max += amplitude;
+
+            amplitude *= m_Gain;
+            frequency *= m_Lacunarity;
+        }
+
+        return sum / max;
     }
 
 private:
     PerlinNoise<T> m_PerlinNoise;
+    T m_Scale{ 0 };
+    T m_Offset{ 0 };
 
-    uint32_t m_Octaves;
-    T m_Frequency, m_Amplitude, m_Persistence;
+    uint32_t m_Octaves{ 0 };
+    T m_Gain{ 0 };
+    T m_Lacunarity{ 0 };
 };
--- src/scene/PerlinNoise.h
+++ src/scene/PerlinNoise.h
@@ -5,41 +5,50 @@
 
 #pragma once
 
-#include <vector>
+#include <array>
 #include <algorithm>
 #include <random>
 
 #include <glm/glm.hpp>
 
+// TODO to calculate gradient using a switch case, should be faster
+// #define CALC_GRAD_SWITCH
 
 /**
  * @brief Perlin noise
  *  Ken Perlin. JAVA REFERENCE IMPLEMENTATION OF IMPROVED NOISE. 2004. 
  *  [online]. https://mrl.cs.nyu.edu/~perlin/noise/.
- *  @see Sascha Willems. Vulkan. https://github.com/SaschaWillems/Vulkan.
  */
 template <typename T>
 class PerlinNoise
 {
 public:
-    PerlinNoise()
+    PerlinNoise(uint32_t seed = std::random_device{}())
+        : m_Seed(seed)
     {
-        // Generate random lookup for permutations containing all numbers 
-        //  from 0..255
-        std::vector<uint8_t> permutation;
-        permutation.resize(PerlinNoise::PERMUTATION_COUNT);
-
-        std::iota(permutation.begin(), permutation.end(), 0);
-        std::default_random_engine rndEngine(std::random_device{}());
-        std::shuffle(permutation.begin(), permutation.end(), rndEngine);
-
-        for (uint32_t i = 0; i < PerlinNoise::PERMUTATION_COUNT; ++i)
-        {
-            m_P[i] = m_P[PerlinNoise::PERMUTATION_COUNT + i] = permutation[i];
-        }
+        GeneratePermutations();
     }
 
-    T Noise(T x, T y, T z)
+    uint32_t GetSeed() const { return m_Seed; }
+    /** @brief Just sets the seed, generate the permutations to see diff. */
+    void SetSeed(uint32_t seed) { m_Seed = seed; }
+
+    /** @brief Generate random lookup for permutations containing all numbers
+     * from 0..255 */
+    void GeneratePermutations()
+    {
+        std::iota(m_P.begin(), m_P.begin() + PerlinNoise::PERMUTATION_COUNT, 0);
+
+        std::default_random_engine rndEngine(m_Seed);
+        std::shuffle(m_P.begin(), m_P.begin() + PerlinNoise::PERMUTATION_COUNT,
+                     rndEngine);
+
+        for (uint32_t i = 0; i < PerlinNoise::PERMUTATION_COUNT; ++i)
+            m_P[PerlinNoise::PERMUTATION_COUNT + i] = m_P[i];
+    }
+
+    /** @return Perlin 3D noise value in [-1,1] */
+    T Noise(T x, T y, T z) const
     {
         int32_t X = (int32_t)glm::floor(x) & 255;   // Find unit cube
         int32_t Y = (int32_t)glm::floor(y) & 255;   // that contains a point.
@@ -64,22 +73,21 @@ public:
                                Lerp(u, Grad(m_P[AB + 1], x, y - 1, z - 1),
                                        Grad(m_P[BB + 1], x - 1, y - 1, z - 1))));
     }
-private:
-    static const size_t PERMUTATION_COUNT = 256;
-    uint32_t m_P[512];
 
 private:
-    T Fade(T t)
+    // Fade fnc, qunitic fnc: 6t^5 - 15t^4 + 10t^3
+    constexpr T Fade(T t) const
     {
         return t * t * t * (t * (t * (T)6 - (T)15) + (T)10);
     }
 
-    T Lerp(T t, T a, T b)
+    constexpr T Lerp(T t, T a, T b) const
     {
         return a + t * (b - a);
     }
 
-    T Grad(int hash, T x, T y, T z)
+#ifndef CALC_GRAD_SWITCH
+    constexpr T Grad(int hash, T x, T y, T z) const
     {
         // Convert LO 4 bits of hash code into 12 gradient directions
         int h = hash & 15;
@@ -87,5 +95,37 @@ private:
         T v = h < 4 ? y : h == 12 || h == 14 ? x : z;
         return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
     }
+#else
+    /** @see http://riven8192.blogspot.com/2010/08/calculate-perlinnoise-twice-as-fast.html */
+    constexpr T Grad(int hash, T x, T y, T z) const
+    {
+        switch(hash & 0xF)
+        {
+            case 0x0: return  x + y;
+            case 0x1: return -x + y;
+            case 0x2: return  x - y;
+            case 0x3: return -x - y;
+            case 0x4: return  x + z;
+            case 0x5: return -x + z;
+            case 0x6: return  x - z;
+            case 0x7: return -x - z;
+            case 0x8: return  y + z;
+            case 0x9: return -y + z;
+            case 0xA: return  y - z;
+            case 0xB: return -y - z;
+            case 0xC: return  y + x;
+            case 0xD: return -y + z;
+            case 0xE: return  y - x;
+            case 0xF: return -y - z;
+            default: return 0; // never happens
+        }
+    }
+#endif // CALC_GRAD_SWITCH
+
+private:
+    static const size_t PERMUTATION_COUNT = 256;
+
+    std::array<uint32_t, PERMUTATION_COUNT*2> m_P;    ///< Permutations
+    uint32_t m_Seed{ 0 };
 };
 
